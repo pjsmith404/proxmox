da1992738a6a98c63feaa5ad0d3f66316ab7dbca
pjsmith404 <pjsmith404@gmail.com>
2026-09-23 21:24:21 +1000

    Change output name

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "nginx_ipv6" {
   value       = proxmox_virtual_environment_vm.nginx.ipv6_addresses[1]
 }
 
-output "instance_ipv6" {
+output "cloud_init_test_ipv6" {
   description = "IPv6 addresses"
   value       = proxmox_virtual_environment_vm.cloud-init-test.ipv6_addresses[1]
 }
